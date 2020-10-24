7be70281ce3f7dae1cbd920faf16e688d72fa753
Ariel Wilson <ariel.wilson.jc@gmail.com>
2020-10-24 15:46:16 -0500

    Updates

---
+++ precache-manifest.6bbaad342336df7df3acfd0f096704bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ac53848578e0b122b07e5b1d991a29",
+    "url": "/mua-site/index.html"
+  },
+  {
+    "revision": "a6dc334299cd72bd70ab",
+    "url": "/mua-site/static/css/main.812cf8a3.chunk.css"
+  },
+  {
+    "revision": "d33836a2b02d2abafbcf",
+    "url": "/mua-site/static/js/2.01efb755.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/mua-site/static/js/2.01efb755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6dc334299cd72bd70ab",
+    "url": "/mua-site/static/js/main.12b1b921.chunk.js"
+  },
+  {
+    "revision": "c48628b097efa0917e0d",
+    "url": "/mua-site/static/js/runtime-main.d49ea891.js"
+  }
+]);
